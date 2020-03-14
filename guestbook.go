7613fe91b74a799b854441a2dabe803d1325bbf7
James Haughey <jimmyhaugh@googlemail.com>
2020-03-14 18:54:38 +0000

    write requests to .txt

--- guestbook.go
+++ guestbook.go
@@ -49,8 +49,33 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	fields := []string{"Name", "Age", "Occupation", "Location"}
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, fields)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("Name")
+	age := request.FormValue("Age")
+	occuptation := request.FormValue("Occupation")
+	location := request.FormValue("Location")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature, age, occuptation, location)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
